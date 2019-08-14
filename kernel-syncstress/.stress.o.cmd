6f918e0236274591dc4efbdfe725ff948d994fc5
Sanidhya Kashyap <sanidhya.k@gmail.com>
2019-08-13 23:48:10 -0400

    remove unnecessary files

--- kernel-syncstress/.stress.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/sanidhya/workspace/research/projects/numa-qspinlock/src/micro-kernel/stress.o := ld -m elf_x86_64 -z max-page-size=0x200000   -r -o /home/sanidhya/workspace/research/projects/numa-qspinlock/src/micro-kernel/stress.o /home/sanidhya/workspace/research/projects/numa-qspinlock/src/micro-kernel/sync-stresser.o 
--- kernel-syncstress/.tmp_versions/stress.mod
+++
@@ -1,3 +0,0 @@
-/home/sanidhya/workspace/research/projects/numa-qspinlock/src/micro-kernel/stress.ko
-/home/sanidhya/workspace/research/projects/numa-qspinlock/src/micro-kernel/sync-stresser.o
-
